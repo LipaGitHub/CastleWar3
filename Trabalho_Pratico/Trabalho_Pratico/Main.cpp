da900188b76e40b771ee7b54f48141b58ccecc4e
Filipa Sousa <lipapsousa@hotmail.com>
2016-12-06 11:06:02 +0000

    Array

--- Trabalho_Pratico/Trabalho_Pratico/Main.cpp
+++ Trabalho_Pratico/Trabalho_Pratico/Main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
-
+#include"Planicie.h"
 using namespace std;
 
 int main() {
-
+	int linha, coluna;
 	cout << "Bem Vindo ao Castle War!!!" << endl;
-
+	cout << "Entre com as dimenoes da planicie" << endl;
+	cin >> linha >> coluna;
+	planicie jogo(linha, coluna);
+	cout << jogo;
 	return 0;
 }
--- Trabalho_Pratico/Trabalho_Pratico/Planicie.h
+++ Trabalho_Pratico/Trabalho_Pratico/Planicie.h
@@ -1,7 +1,33 @@
 #pragma once
 #include "Personagem.h"
+#include <sstream>
 class planicie {
 	//existem colonias, seres e edificios
-	personagem ** mapa[100][100];
-
+	personagem* ** mapa; // 1º *-> ponteiro para personagem  2º *-> 1D 3º *-> 2D
+	int linha, coluna;
+public:
+	planicie(int l, int c) {
+		this->linha = l;
+		this->coluna = c;
+		mapa = new personagem **[l] ;
+		for (int i = 0; i < l; i++) {
+			mapa[i] = new personagem *[c];
+		}
+		for (int i = 0; i < l; i++)
+			for (int j = 0; j < c; j++)
+				mapa[i][j] = NULL;
+	}
+	string toString()const {
+		ostringstream os;
+		for (int i = 0; i < linha; i++) {
+			for (int j = 0; j < coluna; j++)
+				cout << "*";
+			cout << "\n";
+		}
+		return os.str();
+	}
 };
+ostream &operator <<(ostream &o, const planicie &p) {
+	o << p.toString();
+	return o;
+}
